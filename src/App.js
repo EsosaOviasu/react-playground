e9296015695f32d0271666567632bcd2ca70a4fd
Esosa Oviasu <koopaoviasu@gmail.com>
2020-07-13 06:22:55 -0400

    conditional rendering checkpoint

--- src/App.js
+++ src/App.js
@@ -4,6 +4,17 @@ import Messages from './Messages';
 import TheDate from './state/TheDate';
 import Counter from './state/Counter';
 import HellowWorld from './state-drills/HelloWorld';
+import Tabs from './state/Tabs';
+
+
+const tabsProp = [
+  { name: 'First tab',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque.' },
+  { name: 'Second tab',
+    content: 'Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit.' },
+  { name: 'Third tab',
+    content: 'Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur.' },
+];
 
 class App extends Component {
   render() {
@@ -16,6 +27,7 @@ class App extends Component {
         <TheDate />
         <Counter count={123} />
         <HellowWorld />
+        <Tabs tabs={tabsProp}/>
       </div>
     );
   }
