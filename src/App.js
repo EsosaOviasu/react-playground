e368b52f13378351dcf4c1b3e43e8ec298f2fa60
Esosa Oviasu <koopaoviasu@gmail.com>
2020-07-19 09:28:59 -0400

    gwenyth stuf

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import TheDate from './state/TheDate';
 import Counter from './state/Counter';
 import HellowWorld from './state-drills/HelloWorld';
 import Tabs from './state/Tabs';
+import Accordion from './state-drills/Accordion';
 
 
 const tabsProp = [
@@ -15,6 +16,21 @@ const tabsProp = [
   { name: 'Third tab',
     content: 'Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur.' },
 ];
+
+const sectionsProp = [
+  {
+    title: 'Section 1',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+  },
+  {
+    title: 'Section 2',
+    content: 'Cupiditate tenetur aliquam necessitatibus id distinctio quas nihil ipsam nisi modi!',
+  },
+  {
+    title: 'Section 3',
+    content: 'Animi amet cumque sint cupiditate officia ab voluptatibus libero optio et?',
+  },
+]
 
 class App extends Component {
   render() {
@@ -28,6 +44,7 @@ class App extends Component {
         <Counter count={123} />
         <HellowWorld />
         <Tabs tabs={tabsProp}/>
+        <Accordion sections={sectionsProp}/>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { BrowserRouter } from "react-router-dom";
+import App from './paltrows-power-toes/src/paltrows-toes/App';
 import './index.css';
 import HellowWorld from './state-drills/HelloWorld';
 import Bomb from './state-drills/Bomb';
 import RouletteGun from './state-drills/RouletteGun';
+import DemonynApp from './demonymapp/demonymApp'; //Not Working//
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <BrowserRouter>
+        <App/>
+    </BrowserRouter>,
+    document.getElementById('root'));
