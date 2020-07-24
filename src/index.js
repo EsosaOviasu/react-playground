4fee82747d2e3782bd5e12816ab828beab044f2b
Esosa Oviasu <koopaoviasu@gmail.com>
2020-07-24 12:07:27 -0400

    forms validation and error boundaries

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from "react-router-dom";
-import App from './paltrows-power-toes/src/paltrows-toes/App';
+//import App from './paltrows-power-toes/src/paltrows-toes/App';
 import './index.css';
+import AppLang from './lang-context/AppLang';
 import HellowWorld from './state-drills/HelloWorld';
 import Bomb from './state-drills/Bomb';
 import RouletteGun from './state-drills/RouletteGun';
 import DemonynApp from './demonymapp/demonymApp'; //Not Working//
+import RegistrationForm from './registrationForm/registrationForm';
+import App from './playingwithdanger/App';
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App/>
-    </BrowserRouter>,
+    <App />,
     document.getElementById('root'));
